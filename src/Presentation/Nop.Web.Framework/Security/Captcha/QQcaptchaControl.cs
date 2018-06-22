6358e6d57f8a81189b0e3f235f8558cc2380ff6d
大头 <cantain@gmail.com>
2018-06-22 16:36:17 +0800

    样式修改

--- src/Presentation/Nop.Web.Framework/Security/Captcha/QQcaptchaControl.cs
+++ src/Presentation/Nop.Web.Framework/Security/Captcha/QQcaptchaControl.cs
@@ -33,7 +33,7 @@ namespace Nop.Web.Framework.Security.Captcha
             scriptCallbackTag.Attributes.Add("type", MimeTypes.TextJavascript);
             var callback = string.IsNullOrWhiteSpace(Callback) ? "" : Callback + "(res);";
             scriptCallbackTag.InnerHtml.AppendHtml(
-                $"var qq_Captcha_Callback =  function(res){{ \r\nif(res.ret === 0){{ \r\n$(\"input[name='qq_captcha_ticket_field']\").val(res.ticket); \r\n$(\"input[name='qq_captcha_rand_str_field']\").val(res.randstr); \r\n$('#{Id}').html('验证成功')}} \r\n{callback}}}");
+                $"var qq_Captcha_Callback =  function(res){{ \r\nif(res.ret === 0){{ \r\n$(\"input[name='qq_captcha_ticket_field']\").val(res.ticket); \r\n$(\"input[name='qq_captcha_rand_str_field']\").val(res.randstr); \r\n$('#{Id} p').html('验证成功');\r\n$('#{Id}').addClass('tcaptcha-trigger--success');}} \r\n{callback}}}");
             var ticketTag = new TagBuilder("input")
             {
                 TagRenderMode = TagRenderMode.Normal
@@ -54,14 +54,39 @@ namespace Nop.Web.Framework.Security.Captcha
             captchaTag.Attributes.Add("id", Id);
             captchaTag.Attributes.Add("data-appid", Aid);
             captchaTag.Attributes.Add("data-cbfn", "qq_Captcha_Callback");
-            captchaTag.InnerHtml.Append("点击验证");
+            captchaTag.InnerHtml.AppendHtml("<p>点击开始验证</p>");
 
 
-            var styleTag = new TagBuilder("style")
+           var styleTag = new TagBuilder("style")
             {
                 TagRenderMode = TagRenderMode.Normal
             };
-            styleTag.InnerHtml.AppendHtml($"#{Id}{{ width:200px;height:50px;background:lightblue;}}");
+            styleTag.InnerHtml.AppendHtml($@"
+#{Id} {{
+	height: 50px;
+	width:400px;
+	cursor: pointer;
+	margin: 0 auto;
+    position: relative;
+	background: linear-gradient(0deg,hsla(0,0%,93%,.6),hsla(0,0%,100%,0));
+	border: 1px solid rgb(231, 231, 231);
+    position: relative;
+}}
+
+#{Id}:hover {{
+    background: linear-gradient(180deg,hsla(0,0%,93%,.6),hsla(0,0%,100%,0))
+}}
+
+#{Id} p {{
+    font-size: 15px;
+    position: absolute;
+    left: 60px;
+    line-height: 50px;
+}}
+.tcaptcha-trigger--success{{
+    color: #2bc302;
+}}
+");
 
             var scriptLoadApiTag = new TagBuilder("script")
             {
@@ -71,7 +96,7 @@ namespace Nop.Web.Framework.Security.Captcha
 
 
             return scriptLoadApiTag.RenderHtmlContent() + "\r\n" 
-                //+ styleTag.RenderHtmlContent() + "\r\n"
+                + styleTag.RenderHtmlContent() + "\r\n"
                 + ticketTag.RenderHtmlContent() + "\r\n"
                 + randStrTag.RenderHtmlContent() + "\r\n"
                 + scriptCallbackTag.RenderHtmlContent() + "\r\n"
